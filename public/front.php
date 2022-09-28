ed5b2dbaba1230e6449ce6a092018903b37eb8ab
xiongbiao <xiongbiao@yilu.co>
2022-09-28 15:17:40 +0800

    1. 引入http-foundation 2. 引入routing机制 3.引入http-kernel 实现了最基本的控制器,路由分发

---
+++ public/front.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing;
+
+
+$request = Request::createFromGlobals();
+$routes = include __DIR__.'/../src/app.php';
+
+$context = new Routing\RequestContext();
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
+$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
+
+$response->send();
---
+++ src/app.php
@@ -0,0 +1,11 @@
+<?php
+use Symfony\Component\Routing;
+
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
+)));
+
+return $routes;
